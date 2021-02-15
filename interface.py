72658bb6b103ff596224666991c66ac1b8ae1ca4
254Davidhashisoma <davidhashisoam95@gmail.com>
2021-02-15 22:40:45 +0300

    Function to save a new user

---
+++ interface.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.6
+from passlock import User, Credentials
+
+def function():
+	print(" password locker   ")
+function()
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user
+
+def save_user(user):
+    '''
+    Function to save a new user
+    '''
+    user.save_user()

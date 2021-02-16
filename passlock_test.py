98ffe82a1cd8607681e8189fabd65a96dc594fb9
254Davidhashisoma <davidhashisoam95@gmail.com>
2021-02-16 12:28:57 +0300

    test methods run

---
+++ passlock_test.py
@@ -0,0 +1,13 @@
+import unittest
+from passlock import User
+from passlock import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('DavidHashisoma','XyZ3thf1')
